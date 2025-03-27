afed7cf5ea89a0f3e75dc716375d541048800a71
urielo77 <uruiz7794@gmail.com>
2025-03-27 13:57:04 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,21 @@
 # Practica-62
+
+#include <stdio.h>
+
+void main ()
+
+{
+    int a,b; 
+    
+    printf("Dame un numero:");
+    scanf("%int", &a);
+    printf("Dame otro numero:");
+    scanf("%int", &b);
+    
+    if (a > b)
+    { 
+        printf("a mayor que b \n");
+    }
+    
+    printf("fin del programa \n"); 
+}
